7ff2ce6aaf73b85613c88cc8db25ab5611439940
marcoduarte21 <marco.duarte@ucr.ac.cr>
2023-06-12 17:33:38 -0600

    adding data annotations in AjusteDeInventarios

--- Proyecto.Model/AjusteDeInventarios.cs
+++ Proyecto.Model/AjusteDeInventarios.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +11,18 @@ namespace Proyecto.Model
 {
     public class AjusteDeInventarios
     {
+        [HiddenInput]
+        [Key]
         public int Id { get; set; }
         public int Id_Inventario { get; set; }
+        [ForeignKey("Id_Inventario")]
+        public virtual Inventarios Inventarios { get; set; }
+        [Display(Name = "Cantidad Actual")]
+        [Required(ErrorMessage = "El campo Cantidad Actual es requerido")]
         public int CantidadActual { get; set; }
         public int Ajuste { get; set; }
         public TipoDeAjuste Tipo { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Observaciones { get; set; }
         public string UserId { get; set; }
         public DateTime Fecha { get; set; }
