6d6cfe5e2c7c760e70b418b70b5152e07e3a503b
marcoduarte21 <marco.duarte@ucr.ac.cr>
2023-06-12 17:45:12 -0600

    adding data annotations in AperturaDeCaja and AjusteDeInventarios

--- Proyecto.Model/AjusteDeInventarios.cs
+++ Proyecto.Model/AjusteDeInventarios.cs
@@ -20,11 +20,15 @@ namespace Proyecto.Model
         [Display(Name = "Cantidad Actual")]
         [Required(ErrorMessage = "El campo Cantidad Actual es requerido")]
         public int CantidadActual { get; set; }
+        [Required(ErrorMessage = "El campo Ajuste es requerido")]
         public int Ajuste { get; set; }
+        [Required(ErrorMessage = "El campo Tipo es requerido")]
         public TipoDeAjuste Tipo { get; set; }
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "El campo Observaciones es requerido")]
         public string Observaciones { get; set; }
         public string UserId { get; set; }
+        [Required(ErrorMessage = "El campo Fecha es requerido")]
         [DataType(DataType.Date)]
         public DateTime Fecha { get; set; }
     }
--- Proyecto.Model/AperturasDeCaja.cs
+++ Proyecto.Model/AperturasDeCaja.cs
@@ -1,12 +1,20 @@
-﻿using System.Security.Principal;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
 
 namespace Proyecto.Model
 {
     public class AperturasDeCaja
     {
+        [Key]
+        [HiddenInput]
         public int Id { get; set; }
         public int UserId { get; set; }
+        [Display(Name = "Fecha de Inicio")]
+        [Required(ErrorMessage = "El campo Fecha de Inicio es requerido")]
         public DateTime FechaDeInicio { get; set; }
+        [Display(Name = "Fecha de Cierre")]
         public DateTime? FechaDeCierre { get; set; }
         public string? Observaciones { get; set; }
         public EstadoDeLaCaja Estado { get; set; }
