dca43308e60c720449295da75765a9edcd3568c1
marcoduarte21 <marco.duarte@ucr.ac.cr>
2023-06-13 12:48:17 -0600

    agregando metodos de las ventas

--- Proyecto.BL/IServicesComercio.cs
+++ Proyecto.BL/IServicesComercio.cs
@@ -1,7 +1,13 @@
-﻿namespace Proyecto.BL
-{
-    public class IServicesComercio
-    {
+﻿using Microsoft.AspNetCore.Routing.Constraints;
 
+namespace Proyecto.BL
+{
+    public interface IServicesComercio
+    {
+        public void AgregueLaVenta(Model.Ventas venta);
+        public void ElimineElItemDeLaVenta(int id);
+        public Model.Ventas ObtengaLaVentaPorElId(int id);
+        public void ApliqueElDescuento(float porcentajeDeDescuento);
+        public Model.Ventas ProceseLaVenta(Model.Ventas ventas);
     }
 }
---
+++ Proyecto.BL/ServicesComercio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.BL
+{
+    public class ServicesComercio
+    {
+        DA.DBContexto Connection;
+        public ServicesComercio(DA.DBContexto connection)
+        {
+            Connection = connection;
+        }
+
+    }
+}
