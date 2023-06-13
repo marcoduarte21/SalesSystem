144d5f1e7140aadf0dc385532141c304f0b0f7ae
marcoduarte21 <marco.duarte@ucr.ac.cr>
2023-06-12 18:28:50 -0600

    adding DbSets

--- Proyecto.DA/DBContexto.cs
+++ Proyecto.DA/DBContexto.cs
@@ -6,5 +6,16 @@ namespace Proyecto.DA
     public class DBContexto : DbContext
     {
 
+        public DBContexto(DbContextOptions<DBContexto> dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Model.Ventas> Ventas { get; set; }
+        public DbSet<Model.AjusteDeInventarios> AjusteDeInventarios { get; set; }
+        public DbSet<Model.AperturasDeCaja> AperturasDeCaja { get; set; }
+        public DbSet<Model.VentaDetalles> VentaDetalles { get; set; }
+        public DbSet<Model.Inventarios> Inventarios { get; set; }
+
     }
 }
